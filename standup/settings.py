df5818c658b283d50eb5859054bd54526d23d36b
Dave de Fijter <dave@indentity.nl>
2019-10-15 17:18:16 +0200

    debug mode off

--- standup/settings.py
+++ standup/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [os.getenv('DEFAULT_HOST', '127.0.0.1')]
 
