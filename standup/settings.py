6b1bd51d6d5d2ccabc19b4019c7d2da42c3e3859
Dave de Fijter <dave@indentity.nl>
2019-10-15 17:27:43 +0200

    support static files

--- standup/settings.py
+++ standup/settings.py
@@ -237,7 +237,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_PATH), 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 AUTH_USER_MODEL = 'standup.User'
 
